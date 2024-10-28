a49ef60251dbbc6eb0f999d3627ff2bd40a0bc86
jamil9080 <md3711451@gmail.com>
2024-10-28 14:56:28 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Daisy from '../component/DaisyNav/Daisy'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Daisy></Daisy>
+      <h1 className=''>asdjfaf</h1>
+    
+    </>
+  )
+}
+
+export default App
